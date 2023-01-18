468279e9b59e840c2babedb637bccbd25eea1c7f
NotePutip <119995240+NotePutip@users.noreply.github.com>
2023-01-18 11:51:07 +0700

    Create 11statepro.js

---
+++ Lecture05/PROMISE/11statepro.js
@@ -0,0 +1,14 @@
+const Promise = new Promise((resolve, reject) => {
+    const res = true;
+
+    if (res) {
+        resolve("Resolved!");
+    } else {
+        reject(Error("Fatal Error"));
+    }
+});
+
+Promise.then(
+    (res) => console.log(res),
+    (err) => console.log(err)
+);
