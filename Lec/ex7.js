f83d1e1a852c2c493260e912decef4db5956c327
NotePutip <119995240+NotePutip@users.noreply.github.com>
2023-01-25 11:33:03 +0700

    Create ex7.js

---
+++ Lec/ex7.js
@@ -0,0 +1,7 @@
+sumTwoSmalleatNume([19, 5, 42, 2, 77]) > 3
+sumTwoSmalleatNume([10, 343445353, 3453445, 34534545353453]) > 3453455
+sumTwoSmalleatNume([2, 9, 5, -1]) > 8
+sumTwoSmalleatNume([879, 953, 694, -847, 342, 221, -91, -723, 791, -507])
+sumTwoSmalleatNume([3683, 2902, 3951, -479, 1617, -2305]) > 4519
+
+
