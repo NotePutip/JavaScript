b6e30a8580ca06722e7d278c5496909952f1639d
NotePutip <119995240+NotePutip@users.noreply.github.com>
2023-01-25 11:33:19 +0700

    Create ex8.js

---
+++ Lec/ex8.js
@@ -0,0 +1,8 @@
+dihitalClocl(5025) > "01:23:45"
+// 5025 seconds is 1 hour 23 mine 45 sece.
+
+dihitalClocl(6120) > "17:00:01"
+// no am/pm 24h format.
+
+dihitalClocl(87000) > "00:10:00"
+// it a 00:10 next day.
